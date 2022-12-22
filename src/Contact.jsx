7b1db6bb7c301a8ac56281c72ec993d5c4960e6e
RobynSpaulding <robynm27@gmail.com>
2022-12-21 19:51:34 -0800

    styles contact form and removes unneeded code from template and edits CTA section to be a contact reminderlink in index.html

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -24,33 +24,45 @@ export function Contact() {
           console.log(process.env.REACT_APP_SERVICE_ID);
         }
       );
+    e.target.reset();
   };
 
   return (
-    <div id="contact-form">
-      <h1>Contact me for more information</h1>
-      <form ref={form} onSubmit={sendEmail}>
-        <div>
-          <label>Your Name: </label>
-          <input type="text" name="user_name" />
+    <div id="contact-form" class="contact-form">
+      <div class="container" data-aos="fade-up">
+        <div class="section-title">
+          <h1>Contact</h1>
+          <p>
+            Send an email with your name, email address, phone number and the type of work you are looking to get done
+            and I will get back to you to schedule a time to talk.
+          </p>
         </div>
-        <div>
-          <label>Your Email: </label>
-          <input type="email" name="user_email" />
+
+        <div class="col-lg-8 mt-5 mt-lg-0">
+          <form ref={form} onSubmit={sendEmail}>
+            <div class="row gy-2 gx-md-3">
+              <div class="col-md-6 form-group">
+                <input type="text" name="user_name" class="form-control" placeholder="Your Name" required />
+              </div>
+              <div class="col-md-6 form-group">
+                <input type="email" name="user_email" class="form-control" placeholder="Email" required />
+              </div>
+              <div class="col-md-6 form-group">
+                <input type="text" name="user_phone" class="form-control" placeholder="Phone number" required />
+              </div>
+              <div class="col-md-6 form-group">
+                <input type="text" name="subject" class="form-control" placeholder="Subject" />
+              </div>
+              <div class="form-group col-12">
+                <textarea name="message" class="form-control" rows="5" placeholder="Message" required></textarea>
+              </div>
+              <div class="text-center col-12">
+                <input type="submit" value="Send Message" />
+              </div>
+            </div>
+          </form>
         </div>
-        <div>
-          <label>Your Phone Number: </label>
-          <input type="text" name="user_phone" />
-        </div>
-        <div></div>
-        <div>
-          <label>Message: </label>
-          <textarea name="message" />
-        </div>
-        <div>
-          <input type="submit" value="Send" />
-        </div>
-      </form>
+      </div>
     </div>
   );
 }
